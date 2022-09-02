7d7be91b40c5effb54bc677d66ee33eacb5be72e
dicook <visnut@gmail.com>
2022-09-02 11:08:27 +1000

    start on package structure

---
+++ R/givens.R
@@ -0,0 +1,75 @@
+#' Make a d-dimenionsal preprojection space
+#'
+#' @param current starting pxd frame
+#' @param target ending pxd frame
+#'
+#' @return B pre-projection 2dxd matrix 
+#' @export
+#'
+#' @examples
+preprojection <- function(Fa, Fz) {
+  # check both are matrices are both correct size
+  # check each is orthonormal
+  # stopifnot with message
+  Fz_star <- tourr::orthonormalise_by(Fa, Fz)
+  B <- cbind(Fa, Fz_star)
+  return(B)
+}
+
+#' Construct starting basis from pre-projection
+#'
+#' @param Fa starting pxd frame
+#' @param B pre-projection 2dxd matrix 
+#'
+#' @return Wa starting 2dxd frame on preprojection space
+#' @export
+#'
+#' @examples
+construct_Wa <- function(Fa, B) {
+  Wa <- t(B) %*% Fa
+  return(Wa)
+}
+
+#' Construct starting basis from pre-projection
+#'
+#' @param Fz starting pxd frame
+#' @param B pre-projection 2dxd matrix 
+#'
+#' @return Wz starting 2dxd frame on preprojection space
+#' @export
+#'
+#' @examples
+construct_Wz <- function(Fz, B) {
+  Wz <- t(B) %*% Fz
+  return(Wz)
+}
+
+#' Compute angle between two d-dimensional frames in p-space
+#'
+#' @param F1 pxd frame
+#' @param F2 pxd frame
+#'
+#' @return tau angle in radians
+#' @export
+#'
+#' @examples
+calculate_tau <- function(F1, F2) {
+  # This needs to be generalised to frames instead of vectors
+  # calculate the angle between 2 vectors 360 degrees
+  tau <- atan2(F1[2], F1[1]) - atan2(F2[2], F2[1]) 
+  return(tau)
+}
+
+compute_rotation <- function(Wa, tau, nsteps, stepfraction) {
+  # For now this will be a single rotation matrix
+  # but at some generalised
+  # this should compute an increment 
+  # apply k (nsteps) times
+}
+
+revert_to_original <- function() {
+  # Fa = BWa
+  # Fz = BWz increment
+}
+
+
