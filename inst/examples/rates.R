59999a193a42a1b6c0a59f3d15681c6924374d0a
dicook <visnut@gmail.com>
2022-11-18 11:14:46 +1100

    rates example code

---
+++ inst/examples/rates.R
@@ -0,0 +1,24 @@
+# Exploring the currency cross-rates data
+library(tourr)
+library(tidyverse)
+library(GGally)
+
+# Read data
+rates <- read_csv("data-raw/rates_Nov19_Mar20.csv")
+rates <- rates %>% select(date, ARS, AUD, EUR, JPY, KRW, MYR)#, NZD, GBP)
+#rates <- rates %>% select(date, ARS, AUD, EUR, JPY, MYR, ISK)#, NZD, GBP)
+animate_xy(rates[,-1])
+animate_xy(rates[,-1], tour_path = guided_tour(splines2d()))
+
+# Check PCA: why doesn't PCA detect correlation
+# Correlation is between NZD and GBP
+rates_pca <- prcomp(rates[,-1], scale. = TRUE)
+ggscatmat(rates_pca$x)
+
+# Check standardised data - not necessary
+# rates_std <- apply(rates[,-1], 2, function(x) (x-mean(x))/sd(x))
+# animate_xy(rates_std, tour_path = guided_tour(splines2d()))
+
+# Data checks
+ggplot(rates, aes(x=NZD, y=GBP)) + geom_point()
+ggplot(rates, aes(x=NZD, y=AUD)) + geom_point()
